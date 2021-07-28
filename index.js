9dfce801623ba510b88c1f295cec541c4b6f304f
fopingn <fopingn@gmail.com>
2021-07-28 11:09:50 -0400

    index modified from github event rule accross account

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2! with github hook')
+  res.send('CICD App V2! with github hook event rule add')
 })
 
 app.get('/status', (req, res) => {
