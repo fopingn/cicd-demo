d98641151e373fadc074fcf9c59e0d41f12b1b61
fopingn <fopingn@gmail.com>
2021-08-02 09:10:58 -0400

    first dev branch commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD Careveo POC from Vscode add at 29 july and test draining task')
+  res.send('From Dev branch CICD Careveo POC from Vscode add at 2 august and test draining task')
 })
 
 app.get('/status', (req, res) => {
