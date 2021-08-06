f9b50d51149ec90310cf6eab955f38f0b0eb3a81
fopingn <fopingn@gmail.com>
2021-08-06 14:44:46 -0400

    CICD Careveo POC from Vscode add at 06 august - Test with ECR Source after cicd clone delete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD Careveo POC from Vscode add at 06 august - Test with ECR Source')
+  res.send('CICD Careveo POC from Vscode add at 06 august - Test with ECR Source after deleting cicd clone')
 })
 
 app.get('/status', (req, res) => {
